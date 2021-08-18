a0c8f6b56d3cd05f3798a6d43e0a354e611704a1
221geek <abdoulazizdione1997@gmail.com>
2021-08-18 14:14:04 +0000

    build: components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductDetails from "./containers/ProductDetail";
+import ProductListing from "./containers/ProductListing";
 
 function App() {
   return (
     <div className="App">
-      <h1>Helllo</h1>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productID" exact component={ProductDetails} />
+          <Route>404 not found !</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
